3a469f7636f32ae57bfba3ca5c2bad049860beef
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-22 09:17:28 -0800

    add async api in js

--- src/doctor.js
+++ src/doctor.js
@@ -1,1 +1,12 @@
+export class Doctor {
+  async getProfile(){
+    try {
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?last_name=${lastname}&state=${state}&practices=${practice}&user_key=${process.env.API_KEY}`);
+      let jsonifiedResponse = await response.json();
+      return jsonifiedResponse;
+    } catch(error) {
+      console.error("There was an error handling your request: " + error.message);
 
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import {} from './../src/doctor.js';
+
+
+$(document).ready(function(){
+  $('').click(function(){
+    
+  })
+})
