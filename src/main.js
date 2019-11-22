0908dd11ea93f425f71d1d9b5ab8413142657496
DominiqueYoumans <ddyoumans@icloud.com>
2019-11-22 10:40:28 -0800

    add state search in html and imports to main js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import {} from './../src/doctor.js';
-
+import { Doctor } from './../src/doctor.js';
+import $ from 'jquery';
+import 'bootstrap';
+import './styles.css';
 
 $(document).ready(function(){
   $('').click(function(){
     const inputSearch = $("").val();
-  })
-})
+
+    (async () => {
+      let grabInfo = new Doctor();
+      const info = await grabInfo.getPractice(info);
+    })();
+
+    function getPractice(info) {
+      $('#doctorSearch').text(`Practice: ${response.info.}`);
+    };
+
+  });
+});
