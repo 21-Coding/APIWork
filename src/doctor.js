4ef00cb1d73be97ab43462f494a98f100f74806c
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-08 12:52:23 -0800

    add removal of 401 not authoried by changing api fetch call around, now receiving 400 bad api call message

--- src/doctor.js
+++ src/doctor.js
@@ -1,7 +1,7 @@
 export class Doctor {
   async getProfile(doctor){
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctor}&location=or-portland&user_key${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?doctor=${doctor}&location=or-portland&user_key=93ecded497254529c0dd9988fbf5e33b`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import 'bootstrap';
 $(document).ready(function(){
   $('#button').click(function(){
     const doctor = $("#doctorLookup").val();
+    $("#doctorLookup").val("");
 
 
 
@@ -19,12 +20,8 @@ $(document).ready(function(){
 
 
     function getPractice(info) {
-      $('.practiceInfo').text(`Practice Info: ${info.data[0].practices[0]}, ${info.data[0].practices.visit_address.street}, ${info.data[0].practices.visit_address.state},
-      ${info.data[0].practices.visit_address.city},
-      ${info.data[0].practices.phones[0].number},
-      ${info.data[0].practices.phones[0].type},
-      ${info.data[0].practices[0].location_slug}`);
-      // $('.practiceInfo').text(`Practice: $.info.}, Current License Number: ${response.licenses[0].number}`);
+      $('.practiceInfo').text(`Practice Info: ${info.data[0].name}, ${info.data[0].practices.visit_address.street}`); 
+
     }
   });
 });
