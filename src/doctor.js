09d1a9a2df9d3801566cc30b582fb4a95d3d5b9c
DominiqueYoumans <ddyoumans@icloud.com>
2019-12-08 11:49:33 -0800

    add env file, add changes in main js and doctor js

--- src/doctor.js
+++ src/doctor.js
@@ -1,12 +1,12 @@
 export class Doctor {
-  async getProfile(){
+  async getProfile(doctor){
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?practices=${practice}&user_key=1878e7cd5c8d528806fab8600c272574&location=${location}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${doctor}&location=or-portland&user_key${process.env.API_KEY}`);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
       console.error("There was an error handling your request: " + error.message);
 
     }
-  }
-}
+  };
+};
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import { Doctor } from './../src/doctor.js';
 import $ from 'jquery';
 import 'bootstrap';
-import './styles.css';
+
 
 $(document).ready(function(){
   $('#button').click(function(){
-    const stateCity = $("#stateCity").val();
-    $("#stateCity").val("");
-    // const inputSearch = $("#inputSearch").val();
-    // const inputState = $("#inputState").val();
+    const doctor = $("#doctorLookup").val();
+
+
 
 
 
     (async () => {
       let grabInfo = new Doctor();
-      const info = await grabInfo(info).getProfile(stateCity);
+      const info = await grabInfo.getProfile(doctor);
       getPractice(info);
     })();
 
@@ -24,7 +23,8 @@ $(document).ready(function(){
       ${info.data[0].practices.visit_address.city},
       ${info.data[0].practices.phones[0].number},
       ${info.data[0].practices.phones[0].type},
-      ${response.data[0].practices[0].location_slug}`);
+      ${info.data[0].practices[0].location_slug}`);
       // $('.practiceInfo').text(`Practice: $.info.}, Current License Number: ${response.licenses[0].number}`);
     }
   });
+});
